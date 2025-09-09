4d7ed5a733426c4144e6e86dbf1fc43bcb11f02c
Danilzs <danzone00@gmail.com>
2025-09-09 15:02:51 -0300

    Herança - criando a classe filha Manager

--- heranca/src/Employee.java
+++ heranca/src/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
     private String name;
     private String address;
     private int age;
+    private double salary;
 
     public String getCode() {
         return code;
@@ -35,5 +36,13 @@ public class Employee {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 }
