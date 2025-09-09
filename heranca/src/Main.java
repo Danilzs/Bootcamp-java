2f837ef0428de123352c743a88ba19519e4de039
Danilzs <danzone00@gmail.com>
2025-09-09 15:17:23 -0300

    Herança - criando a classe filha cliente

--- heranca/src/Main.java
+++ heranca/src/Main.java
@@ -5,5 +5,12 @@ public class Main {
         Employee employee = new Employee();
         Manager manager = new Manager();
 
+        manager.setName("Daniel");
+        manager.setLogin("Daniel");
+        manager.setPassword("1234567");
+
+        System.out.println(manager.getName());
+        System.out.println(manager.getLogin());
+        System.out.println(manager.getPassword());
         }
     }
